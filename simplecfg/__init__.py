a2a4560ce8999595719a2984ed6af0a131c39c6a
itsmaxymoo <max.loiacono@protonmail.com>
2022-03-25 13:07:51 -0400

    readme update

--- simplecfg/__init__.py
+++ simplecfg/__init__.py
@@ -1,7 +1,7 @@
 from simplecfg.config import Config
 
 MODULE_NAME = "simplecfg"
-MODULE_NAME_FULL = "Simple CFG"
+MODULE_NAME_FULL = "SimpleCFG"
 MODULE_VERSION = "2.0.0"
 MODULE_AUTHOR = "Max Loiacono"
 MODULE_DESCRIPTION = "A library to easily manage program configuration."
